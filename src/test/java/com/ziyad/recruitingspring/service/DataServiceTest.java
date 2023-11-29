985910996bf91e8a496eaec932e05f7d21f0b2c2
Ziyad <137479354+ZiyadBoshima@users.noreply.github.com>
2023-11-29 12:39:11 +0100

    test: add test case for doc to text conversion

---
+++ src/test/java/com/ziyad/recruitingspring/service/DataServiceTest.java
@@ -0,0 +1,73 @@
+package com.ziyad.recruitingspring.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class DataServiceTest {
+
+    @InjectMocks private DataService dataService;
+
+    @Mock
+    private GPTService gptService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+    @Test
+    public void testConvertDocToText() {
+        try {
+            Path pdfFilePath = Paths.get("src", "test", "resources", "dummy.pdf");
+
+            InputStream pdfInputStream = Files.newInputStream(pdfFilePath);
+
+            MockMultipartFile mockFile = new MockMultipartFile("dummy.pdf", pdfInputStream);
+
+            String result = dataService.convertDocToText(mockFile);
+            assertEquals("Dummy PDF file\n", result);
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("An exception occurred while testing convertDocToText.");
+        }
+    }
+
+    @Test
+    public void testExtractJsonWithData() {
+        String options = "name, years of experience, and skills";
+        String data = "name steven, experience 2 years, skills carpentry.";
+        String expectedPrompt = "Extract " + options + " in JSON format. Text data: " + data;
+
+        /*
+        List<GPTResponse.Choice> list = new ArrayList<>(GPTResponse.Choice(1, new Message("User", "Response"))
+        GPTResponse gptResponse = new GPTResponse(List<GPTResponse.Choice>().add(1, new Message("User", "Response"));
+        gptResponse.getChoices().add(new GPTResponse.Choice(1, new Message("User", "Response")));
+
+        when(gptService.generateResponse(expectedPrompt)).thenReturn(gptResponse);
+
+        ResponseEntity<String> result = documentService.extractJson(options, data);
+
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertEquals("Response", result.getBody());
+         */
+    }
+}
