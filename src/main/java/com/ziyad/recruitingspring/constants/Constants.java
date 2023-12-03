aa8f858cc610735466a2c5192b98f07c88bf823e
Ziyad <137479354+ZiyadBoshima@users.noreply.github.com>
2023-12-03 13:53:00 +0100

    feat: create job model

--- src/main/java/com/ziyad/recruitingspring/constants/Constants.java
+++ src/main/java/com/ziyad/recruitingspring/constants/Constants.java
@@ -1,6 +1,14 @@
 package com.ziyad.recruitingspring.constants;
 
 public class Constants {
+
     public static final String RESUME_PROCESSING_OPTIONS = "the name, skills (a json array), and years of experience(labeled yearsOfExperience, " +
             "which represents professional experience only and is indicated by one integer value. Years of experience is not an array).";
+    public enum JobType {
+        FULL_TIME, PART_TIME, CONTRACT, INTERNSHIP
+    }
+
+    public enum Remote {
+        ONSITE, REMOTE, HYBRID
+    }
 }
